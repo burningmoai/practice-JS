232bfbe38265e3ca952398e40cebc1784bb2f8a0
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-16 21:55:27 +0000

    [Finish]簡単な関数の記述

---
+++ sample.js
@@ -0,0 +1,9 @@
+//function 関数名(引数){
+//  処理
+//  return 戻り値;
+// }
+
+function addString(strA){ //function(ひとまとまり)、関数名addString、引数strA
+  let addStr = "Hello"+ strA; //変数名addStrへ「Hello」と「strAに設定された文字列」を結合
+  return addStr; //結合結果が入ったaddStr↑を関数の戻り値に設定
+}
