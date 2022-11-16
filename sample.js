a2a7d3d57123fb4ecc82e01cf502422419ed28e4
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-11-16 22:06:47 +0000

    [Add]作成した関数の呼び出し

--- sample.js
+++ sample.js
@@ -1,8 +1,18 @@
+//メイン部分
+let alertString;
+alertString = addString("お嬢ちゃん");
+
+//作成した関数を呼び出す
+alert(alertString);
+
+//関数の書き方
 //function 関数名(引数){
 //  処理
 //  return 戻り値;
 // }
 
+
+//作成した関数
 function addString(strA){ //function(ひとまとまり)、関数名addString、引数strA
   let addStr = "Hello"+ strA; //変数名addStrへ「Hello」と「strAに設定された文字列」を結合
   return addStr; //結合結果が入ったaddStr↑を関数の戻り値に設定
